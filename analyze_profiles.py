d51fa17fbe97240239ff435351dd870644098211
Carlos C <ccordogo@gmail.com>
2025-08-01 11:57:34 +0200

    Comments added for analyze_profiles & config_example

--- analyze_profiles.py
+++ analyze_profiles.py
@@ -13,25 +13,30 @@ from profiles import plot_genre_distribution_histogram
 
 from exploration import print_unique_tags
 
-#carga datos
+#upload data from csv
 df = pd.read_csv("data/steam_data.csv")
 
-#calculo de los perfiles
+#calculate profiles for yee and bartle
 yee_df, bartle_df = compute_profiles(df)
 
-
+#print the listed unique tags of the games
 print_unique_tags(df)
 
+#save bartle compass
 plot_bartle_compass(bartle_df)
 
-print("Jugadores:", list(yee_df.index))
+#save yee radar for each user
+print("User:", list(yee_df.index))
 for player in yee_df.index:
     save_yee_radar_png(yee_df, player)
 
+#print stats for different users
 print_user_stats(df)
 
-
+#save top games and genres graph
 plot_top_games_with_genres(df)
+#save genre distribution grapg
 plot_genre_distribution_histogram(df)
-    
-print("Completado")
+
+#print confirmation of the script end
+print("Completed")
--- config_example.py
+++ config_example.py
@@ -1,2 +1,6 @@
 
+#steam key is personal and can be obtained for free as a registered steam developer at:
+#https://steamcommunity.com/dev/apikey
+
+#once obtained, change the name of the script for config.py
 STEAM_API_KEY = "XXXXXXXXXXXXXXXXXXXXXXXXXXXX"  
