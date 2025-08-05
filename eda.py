2bf040e5dbcb74ddad84635dca3febc17f770b50
Carlos C <ccordogo@gmail.com>
2025-08-05 16:34:38 +0200

    Comments for eda.py

--- eda.py
+++ eda.py
@@ -5,15 +5,19 @@ import seaborn as sns
 import ast
 import numpy as np
 
+
+#upload data from csv and sort by participant id
 df = pd.read_csv("data/steam_data.csv")
 df = df.sort_values(by='participant_id')
 
-
+#convert genres and categories columns to lists
 df['genres'] = df['genres'].apply(lambda x: ast.literal_eval(x) if isinstance(x, str) else [])
 df['categories'] = df['categories'].apply(lambda x: ast.literal_eval(x) if isinstance(x, str) else [])
 
-# ------------------- TOP GÉNEROS -------------------
+# ------------------- TOP GENRES -------------------
+#creates a row for every game genre
 exploded_genres = df.explode('genres')
+#group by genre and sort ny hours played, getting the TOP 10
 top_genres = exploded_genres.groupby('genres')['playtime_hours'].sum().sort_values(ascending=False).head(10)
 
 plt.figure(figsize=(10, 6))
@@ -22,10 +26,14 @@ plt.title('Top 10 géneros más jugados por horas')
 plt.xlabel('Horas jugadas')
 plt.ylabel('Género')
 plt.tight_layout()
+#save as png
 plt.savefig("output/top_generos_grupo.png")
+plt.close()
 
-# ------------------- TOP CATEGORÍAS -------------------
+# ------------------- TOP CATEGORIES -------------------
+#creates a row for every game category
 exploded_categories = df.explode('categories')
+#group by genre and sort ny hours played, getting the TOP 10
 top_categories = exploded_categories.groupby('categories')['playtime_hours'].sum().sort_values(ascending=False).head(10)
 
 plt.figure(figsize=(10, 6))
@@ -34,9 +42,12 @@ plt.title('Top 10 categorías más jugadas por horas')
 plt.xlabel('Horas jugadas')
 plt.ylabel('Categoría')
 plt.tight_layout()
+#save as png
 plt.savefig("output/top_categorias_grupo.png")
+plt.close()
 
-# ------------------- COMPARATIVA HLTB -------------------
+# ------------------- COMPARATIVE HLTB ------------------- (HowLongToBeat)
+#filter games with data from hltb with more than 0 hours
 df_hltb = df[(df['hltb_main_story'].notna()) & (df['hltb_main_story'] > 0)]
 
 plt.figure(figsize=(8, 6))
@@ -55,13 +66,11 @@ plt.xlim(0, df_hltb['hltb_main_story'].max() + 5)
 plt.ylim(0, df_hltb['playtime_hours'].max() + 5)
 plt.savefig("output/plot_hltb.png")
 plt.close()
-print("✅ Guardado: output/plot_hltb.png")
 
 
-# ───── HORAS VS HLTB (FILTRADO) ───── #
-import seaborn as sns
-import matplotlib.pyplot as plt
 
+# ------------------- COMPARATIVE HLTB (FILTERED) ------------------- #
+#filter to games between HLTB in range [+0, 100] and playtime hours  in range [+0, 200]
 df_hltb_filtered = df[
     (df['hltb_main_story'].notna()) &
     (df['hltb_main_story'] > 0) &
@@ -82,50 +91,51 @@ plt.plot([0, 100], [0, 100], linestyle='--', color='grey')
 plt.title('Horas jugadas vs estimación HLTB (filtrado)')
 plt.xlabel('HLTB: Historia principal (horas)')
 plt.ylabel('Horas jugadas reales')
+#axis limitations
 plt.xlim(0, 105)
 plt.ylim(0, 205)
 plt.tight_layout()
 plt.savefig("output/plot_hltb_filtered.png")
 plt.close()
-print("✅ Guardado: output/plot_hltb_filtered.png")
 
 
 
-# ------------------- INSIGHT GENERAL -------------------
+
+# ------------------- GENERAL INSIGHT -------------------
 print("\nResumen exploratorio:")
-print("- Número de juegos totales:", len(df))
-print("- Número de juegos únicos:", df['appid'].nunique())
-print("- Número de juegos compartidos:", len(df)-df['appid'].nunique())
-print("- Número de participantes:", df['participant_id'].nunique())
-print("- Género más jugado:", top_genres.idxmax())
-print("- Categoría más jugada:", top_categories.idxmax())
-print("- Tiempo medio por juego:", round(df['playtime_hours'].mean(), 2), "horas")
+print("- Número de juegos totales:", len(df)) #total number of games
+print("- Número de juegos únicos:", df['appid'].nunique()) #total number of unique games
+print("- Número de juegos compartidos:", len(df)-df['appid'].nunique()) #total number of repeated games
+print("- Número de participantes:", df['participant_id'].nunique()) #number of participants
+print("- Género más jugado:", top_genres.idxmax()) #most played genre
+print("- Categoría más jugada:", top_categories.idxmax()) #most played category
+print("- Tiempo medio por juego:", round(df['playtime_hours'].mean(), 2), "horas") #average game time
 
-mean_played = df[df['playtime_hours'] > 0]['playtime_hours'].mean()
-print("- Tiempo medio por juego (solo si jugado):", round(mean_played, 2), "horas")
+mean_played = df[df['playtime_hours'] > 0]['playtime_hours'].mean() #calculate mean
+print("- Tiempo medio por juego (solo si jugado):", round(mean_played, 2), "horas") #average mean time for played games
 
-num_juegos_0_horas = (df['playtime_hours'] == 0).sum()
-print("- Juegos con 0 horas jugadas:", num_juegos_0_horas)
+num_juegos_0_horas = (df['playtime_hours'] == 0).sum() #calculate 0 hours played games
+print("- Juegos con 0 horas jugadas:", num_juegos_0_horas) #games with 0 hours
 
-num_juegos_jugados = df[df['playtime_hours'] > 0]['appid'].nunique()
-print("- Número de juegos únicos con más de 0 horas:", num_juegos_jugados)
-
-
-juegos_total = df['appid'].unique()
-juegos_jugados = df[df['playtime_hours'] > 0]['appid'].unique()
-juegos_no_jugados = set(juegos_total) - set(juegos_jugados)
-
-print("- Número de juegos únicos con 0 horas:", len(juegos_no_jugados))
+num_juegos_jugados = df[df['playtime_hours'] > 0]['appid'].nunique() #calculate number of unique played games for more than 0 hours
+print("- Número de juegos únicos con más de 0 horas:", num_juegos_jugados) #unique games with +0 hours
 
 
-#--------------------------Top 10 juegos ---------------------------------
+juegos_total = df['appid'].unique() #total unique games
+juegos_jugados = df[df['playtime_hours'] > 0]['appid'].unique() #played unique games
+juegos_no_jugados = set(juegos_total) - set(juegos_jugados) #unique not played games
+
+print("- Número de juegos únicos con 0 horas:", len(juegos_no_jugados)) #unique games with 0 hours
+
+
+# ------------------- Top 10 games -------------------
 
 
 top_games_group = (
-    df.groupby('name_x')['playtime_hours']
-    .sum()
+    df.groupby('name_x')['playtime_hours'] #group by game name
+    .sum() #sum total hours
     .sort_values(ascending=False)
-    .head(10)
+    .head(10) #get TOP 10
 )
 
 plt.figure(figsize=(10, 6))
@@ -135,24 +145,22 @@ plt.xlabel("Horas totales jugadas")
 plt.ylabel("Juego")
 plt.tight_layout()
 plt.savefig("output/top_juegos_grupo.png")
+plt.close()
 
 
-
-#-----------------------------EDA--------------------------------
-
-
-# Filtramos solo juegos jugados (> 0h)
+# ------------------- EDA -------------------
+#filter only played games
 played = df[df['playtime_hours'] > 0]['playtime_hours']
 
-# Estadísticos de dispersión
+#dispersion statistics
 std = played.std()
 mean = played.mean()
-cv = std / mean if mean != 0 else np.nan  # Coeficiente de variación
+cv = std / mean if mean != 0 else np.nan  #coefficient of variation
 percentiles = played.quantile([0.25, 0.5, 0.75, 0.9])
-iqr = percentiles[0.75] - percentiles[0.25]
+iqr = percentiles[0.75] - percentiles[0.25] #interquartile range
 
 
-
+#print statistics
 print("----------------------Estadísticos--------------------------")
 print(f"- Desviación estándar: {std:.2f} horas")
 print(f"- Coeficiente de variación (CV): {cv:.2f}")
@@ -166,18 +174,18 @@ print(f"- Rango intercuartílico (IQR): {iqr:.2f} horas")
 
 
 
-#-----------------------------SIN GÉNERO/CATEGORIA--------------------------------
-sin_genero = 0
-sin_categoria = 0
+# ------------------- NO GENRE/CATEGORY -------------------
+sin_genero = 0 #no genre
+sin_categoria = 0 #no category
 
 for i, row in df.iterrows():
-    #contar genero vacio
+    #count empty genre
     if not row['genres'] or row['genres'] == [] or row['genres'] == "['']":
         sin_genero += 1
 
-    #contar categoria vacia
+    #count empty category
     if not row['categories'] or row['categories'] == [] or row['categories'] == "['']":
         sin_categoria += 1
 
-print(f"Juegos sin género: {sin_genero}")
-print(f"Juegos sin categoría: {sin_categoria}")
+print(f"Juegos sin género: {sin_genero}") #nbo genre
+print(f"Juegos sin categoría: {sin_categoria}") #no category
